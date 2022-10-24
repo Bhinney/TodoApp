dbef252f9d9aefd48d4f1f717b250e69adeba381
Bhinney <107696895+Bhinney@users.noreply.github.com>
2022-10-24 10:00:32 +0900

    feat : implement controller structure (#14)

--- TodoApplication/src/main/java/com/codestates/TodoApplication/controller/TodoHomeController.java
+++ TodoApplication/src/main/java/com/codestates/TodoApplication/controller/TodoHomeController.java
@@ -1,7 +1,7 @@
 package com.codestates.TodoApplication.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TodoHomeController {
@@ -9,5 +9,35 @@ public class TodoHomeController {
     @GetMapping("/")
     public String helloWorld(){
         return "To-Do Application !";
+    }
+
+    // 투두 포스팅
+    @PostMapping
+    public ResponseEntity postTodo(){
+        return null;
+    }
+
+    // 투두 수정
+    @PatchMapping
+    public ResponseEntity patchTodo(){
+        return null;
+    }
+
+    // 투두 조회
+    @GetMapping
+    public ResponseEntity getTodo(){
+        return null;
+    }
+
+    // 투두 페이지 조회
+    @GetMapping
+    public ResponseEntity getTodos(){
+        return null;
+    }
+
+    // 투두 삭제
+    @DeleteMapping
+    public ResponseEntity deleteTodo(){
+        return null;
     }
 }
